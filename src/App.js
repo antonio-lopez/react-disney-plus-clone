610c75605dff1029ca3963b1b693b7a3506f18ae
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-07-14 19:57:54 -0700

    added sign in and sign out along with correct redirects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Header from './components/Header';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
@@ -12,9 +13,11 @@ function App() {
           <Route exact path='/'>
             <Login />
           </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
         </Switch>
       </Router>
-      <h2>hello</h2>
     </div>
   );
 }
